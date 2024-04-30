c42d2dc60f2018d09cc6840e44c7a0225a2b26c5
Long Nguyen <longkakapro2000@gmail.com>
2024-04-30 23:28:33 +0700

    add loadbalancing module

--- deploy-app/module/loadbalancing/main.tf
+++ deploy-app/module/loadbalancing/main.tf
@@ -1,1 +1,31 @@
 # chứa các thông tin định nghĩa service cần tạo
+resource "aws_lb" "three_tier_lb" {
+    name = "three_tier_lb"
+    security_groups = var.lb_sg
+    subnets = var.public_subnets 
+    idle_timeout = 400
+
+    depends_on = [ var.app_sg ]
+}
+
+resource "aws_lb_target_group" "three_tier_tg" {
+    name = "three_tier_lb_tg"
+    port = var.port
+    protocol = var.protocol
+    vpc_id = var.vpc_id
+
+    lifecycle {
+      ignore_changes = [ name ]
+      create_before_destroy = true
+    }
+}
+
+resource "aws_lb_listener" "three_tier_lb" {
+    load_balancer_arn = aws_lb.three_tier_lb.arn
+
+    default_action {
+      type = "forward"
+      target_group_arn = aws_lb_target_group.three_tier_lb.arn
+    }
+
+}
--- deploy-app/module/loadbalancing/variables.tf
+++ deploy-app/module/loadbalancing/variables.tf
@@ -1,1 +1,25 @@
 # dùng để truyền biến vào (có thể biến tự định nghĩa hoặc biến lấy từ output.tf)
+
+variable "lb_sg" {
+  
+}
+
+variable "public_subnets" {
+  
+}
+
+variable "app_sg" {
+  
+}
+
+variable "port" {
+  
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "protocol" {
+  
+}
