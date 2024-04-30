8fffc4c9078011cdfce828194e6325632bf5142a
Long Nguyen <longkakapro2000@gmail.com>
2024-04-30 22:56:42 +0700

    add db module

--- deploy-app/module/database/main.tf
+++ deploy-app/module/database/main.tf
@@ -1,1 +1,21 @@
 # chứa các thông tin định nghĩa service cần tạo
+
+resource "aws_db_instance" "three_tier_db" {
+  
+    instance_class = var.instance_class
+    allocated_storage = var.allocated_storage
+    engine = "mysql"
+    engine_version = var.db_version
+    db_name = var.db_name
+    username = var.db_user
+    password = var.db_password
+    db_subnet_group_name = var.db_subnet_group_name
+    identifier = var.identifier
+    skip_final_snapshot = var.skip_final_snapshot
+    vpc_security_group_ids = [var.rds_sg]
+
+    tags = {
+      Name = "three_tier_db"
+    }
+
+}
--- deploy-app/module/database/variables.tf
+++ deploy-app/module/database/variables.tf
@@ -1,1 +1,40 @@
 # dùng để truyền biến vào (có thể biến tự định nghĩa hoặc biến lấy từ output.tf)
+
+variable "allocated_storage" {
+  
+}
+
+variable "instance_class" {
+  
+}
+
+variable "db_version" {
+  
+}
+
+variable "db_name" {
+  
+}
+variable "db_user" {
+  
+}
+
+variable "db_password" {
+  
+}
+
+variable "db_subnet_group_name" {
+  
+}
+
+variable "identifier" {
+  
+}
+
+variable "skip_final_snapshot" {
+  
+}
+
+variable "rds_sg" {
+  
+}
